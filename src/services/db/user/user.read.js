dcb699450beb66358745e3c4d64e47de68589eb4
Vladislav Rahmanov <rahmanov.official@yandex.ru>
2025-08-13 13:53:18 +0300

    feat: add user admin management features, including methods to list, add, and remove admins, enhancing user role management capabilities

--- src/services/db/user/user.read.js
+++ src/services/db/user/user.read.js
@@ -1,5 +1,5 @@
 import { prisma } from '../prisma.js'
-import { cacheGet } from '#cache'
+import { cacheGet, cacheDel } from '#cache'
 
 export const getUserByTelegramId = async telegramId => {
   return cacheGet(['user', 'byTg', String(telegramId)], async () => {
@@ -22,6 +22,10 @@ export const getUserByTelegramId = async telegramId => {
       }
     })
   }, 60)
+}
+
+export const getUserById = async id => {
+  return prisma.user.findUnique({ where: { id: Number(id) } })
 }
 
 export const getUserStats = async telegramId => {
